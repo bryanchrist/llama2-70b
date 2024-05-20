33d03eeef75fde09c958066ce04f76e586d20785
bryanchrist <brc4cb@virginia.edu>
2024-05-20 13:18:10 -0400

    Generating questions

---
+++ phi3.py
@@ -0,0 +1,86 @@
+import os
+os.environ['TRANSFORMERS_CACHE'] = '/scratch/brc4cb/phi3/cache'
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np 
+import pandas as pd
+import random
+
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-mini-4k-instruct", 
+    device_map="cuda", 
+    torch_dtype="auto", 
+    trust_remote_code=True, 
+)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+
+
+df = pd.read_csv('data/sgsm.csv') # Load SGSM dataset for few-shot prompting
+df = df[df['subset']=="sgsm_train"] # Subset SGSM to verified training subset
+df = df.sample(frac = 1, random_state = 42)
+for i in range(0, len(df)):
+    try:
+        answer = df.iloc[i]['answer']
+        answer = float(answer)
+        df.iloc[i]['answer'] = answer
+    except:
+        df = df.drop([i])
+        
+exec_code = []
+solved = []
+for i in range(0, len(df)-6):
+    # Format the prompt
+    prompts = []
+    questions = []
+    final_question = df.iloc[i]['question']
+    answer = df.iloc[i]['answer']
+    final_prompt = f"""<|user|>\n{final_question} Let's write a Python program. <|end|>\n<|assistant|>"""
+    
+    for j in range(len(df)-5, len(df)):
+        question = df['question'].iloc[j]
+        solution = df['solution'].iloc[j]
+        prompt = f"""<|user|>\n{question} Let's write a Python program. <|end|>\n<|assistant|>\n{solution}\n<|end|>"""
+        if prompt not in prompts:
+            prompts.append(prompt)
+
+    prompts.append(final_prompt)
+    formatted_prompt = "\n".join(prompts)
+
+    #Query the model 
+    inputs = tokenizer.encode(formatted_prompt, return_tensors="pt").to('cuda')
+    output = model.generate(inputs, max_new_tokens = 100)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    # Split the generated text by the prompt to extract the newly generated part
+    generated_text_parts = generated_text.split(final_prompt)
+    solution_text = generated_text_parts[-1].strip()
+    if "<|end|>" in solution_text:
+            solution_text = solution_text.split("<|end|>")[0] # Split up a generation that contains more than one question
+    if "<|user|>" in solution_text:
+            solution_text = solution_text.split("<|user|>")[0] # Split up a generation that contains more than one question
+    try:
+        exec(solution_text)
+        model_answer = solution()
+        exec_code.append(1)
+        model_answer = float(model_answer)
+        if model_answer != answer:
+            solved.append(0)
+                
+
+        if model_answer == answer:
+            solved.append(1)
+
+    except:
+        solved.append(0)
+        exec_code.append(0)
+        pass
+        
+print(f"First attempt solve rate %: {np.mean(solved)*100}")
+print(f"% exec code: {np.mean(exec_code)*100}")
+
+output_file = "phi_test.txt"  # Specify the path and filename for the output file
+
+with open(output_file, "a") as f:  # Open the file in append mode ("a")
+    f.write(f"""-----------First attempt solve rate %: {np.mean(solved)*100}""" + "\n" + f"% exec code: {np.mean(exec_code)*100}" + "\n\n ----")  # Append the newly generated text to the file
+    
